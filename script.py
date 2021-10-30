9af7a0f820b028410b60226477bcbe60d543c9d1
hamidpvr786 <88972894+hamidpvr786@users.noreply.github.com>
2021-10-30 14:53:00 +0530

    Update script.py

--- script.py
+++ script.py
@@ -48,13 +48,13 @@ See <i>/help</i> for commands and more details.</b>
 <code>/info userid</code>  -  Shows User Information. Use <code>/info</code> as reply to some message for their details!
 
 
-<b>© @TroJanzHEX</b>
+<b>© @hd_designer_p</b>
 """
 
 
     ABOUT_MSG = """⭕️<b>My Name : TroJanz Filter Bot</b>
 
-⭕️<b>Creater :</b> @TroJanzHEX    
+⭕️<b>Creater :</b> @hamidpvr786    
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
