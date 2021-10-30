230d085efbb8b3a9dda52779dfccee3928b5befa
hamidpvr786 <88972894+hamidpvr786@users.noreply.github.com>
2021-10-30 13:36:44 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -47,12 +47,12 @@ async def cb_handler(client, query):
         keyboard = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
+                    InlineKeyboardButton("Islamic Group", url="https://t.me/hubburasoolsongs"),
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("Channel", url="https://t.me/hd_designer_p"),
+                    InlineKeyboardButton("Support Group", url="https://t.me/joinchat/xRQO-i85mixjYmNl")
                 ]
             ]
         )
@@ -70,7 +70,7 @@ async def cb_handler(client, query):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                        "SOURCE CODE", url="https://t.me/hd_designer_p/3")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
